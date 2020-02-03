278f78aab9d156af7738771d0521e57f0d4cd101
Victor Sued <visued@gmail.com>
2020-02-03 12:21:50 -0300

    Formulários reativos: Fazendo submit

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-data-form',
@@ -10,7 +12,8 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder,
+              private http: HttpClient) { }
 
   ngOnInit() {
     // this.formulario = new FormGroup({
@@ -20,6 +23,13 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null],
       email: [null]
+    });
+  }
+
+  onSubmit() {
+    this.http.post('//httpbin.org/post', JSON.stringify(this.formulario.value))
+    .subscribe((res) => {
+      console.log(res);
     });
   }
 
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DataFormComponent } from './data-form.component';
 import { ShareModule } from '../share/share.module';
@@ -14,7 +15,9 @@ import { ShareModule } from '../share/share.module';
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    ShareModule
+    ShareModule,
+    HttpClientModule
+
   ]
 })
 export class DataFormModule { }
