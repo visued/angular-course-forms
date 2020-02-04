d4312dbb9b37fd022049db95010dc0e1b7756e71
Victor Sued <visued@gmail.com>
2020-02-04 15:44:15 -0300

    Formulários reativos: Checkbox Toggle

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -54,7 +54,8 @@ export class DataFormComponent implements OnInit {
       }),
       cargo: [null],
       tecnologias: [null],
-      newsletter: ['s']
+      newsletter: ['s'],
+      termos: [null, Validators.pattern('true')]
     });
   }
 
